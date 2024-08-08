217b654b77a2ec4ce4886aa1404312a8f24a1142
chiragjain307 <chiragjain.jain7@gmail.com>
2024-08-09 00:08:08 +0530

    login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -16,13 +16,13 @@ function Login() {
 
       <div className=' relative bg-[#eff2f5] h-[calc(100vh-220px)] w-full flex justify-center'>
         <div className='absolute h-[80%] w-3/4 bg-white top-[-70px] shadow-2xl  rounded-lg flex flex-col justify-center items-center gap-1'>
-          
-            <img src={fingerPrint} alt=''  className='h-20'/>
-            <p>Sign In</p>
-            <p>Sign in with your google account to get started.</p>
-            <button className='bg-[#1c3669] text-white p-2 rounded-lg '>Sign in with Google <span><BiLogIn /></span></button>
-            
-          
+
+          <img src={fingerPrint} alt='' className='h-20' />
+          <p className='mt-4 font-bold text-xl'>Sign In</p>
+          <p className='text-gray-400'>Sign in with your google account to get started.</p>
+          <button className='bg-[#1c3669] text-white p-2 rounded-lg flex items-center gap-1 mt-4'>Sign in with Google<BiLogIn /></button>
+
+
         </div>
       </div>
 
